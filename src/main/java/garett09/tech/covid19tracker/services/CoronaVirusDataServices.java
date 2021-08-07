f6d11b2f43a19d4a919788b2b91780d4e79c156a
Adrian Sian <adriansian@gmail.com>
2021-08-07 16:02:06 +0800

    covid19-tracker: change to Java 12
    Signed-off-by: Adrian Sian <adriansian@gmail.com>

---
+++ src/main/java/garett09/tech/covid19tracker/services/CoronaVirusDataServices.java
@@ -0,0 +1,13 @@
+package garett09.tech.covid19tracker.services;
+
+public class CoronaVirusDataServices {
+
+    private static String VIRUS_DATA_URL= "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+
+    public void fetchVirusData(){
+        HttpClient client = HttpClient.newHttpClient();
+
+
+    }
+
+}
