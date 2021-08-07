77650b36088900ea2a8f1755825cfb07d86787b5
Adrian Sian <adriansian@gmail.com>
2021-08-07 17:06:28 +0800

    covid19-tracker: enable parsing of CSV files
    Signed-off-by: Adrian Sian <adriansian@gmail.com>

--- src/main/java/garett09/tech/covid19tracker/Covid19trackerApplication.java
+++ src/main/java/garett09/tech/covid19tracker/Covid19trackerApplication.java
@@ -2,8 +2,10 @@ package garett09.tech.covid19tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Covid19trackerApplication {
 
 	public static void main(String[] args) {
