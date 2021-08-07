476a174fa95552c536c32d65507de4b96a4e3805
Adrian Sian <adriansian@gmail.com>
2021-08-07 14:39:14 +0800

    Initial commit

---
+++ src/main/java/garett09/tech/covid19tracker/Covid19trackerApplication.java
@@ -0,0 +1,13 @@
+package garett09.tech.covid19tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19trackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19trackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/garett09/tech/covid19tracker/Covid19trackerApplicationTests.java
@@ -0,0 +1,13 @@
+package garett09.tech.covid19tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19trackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
